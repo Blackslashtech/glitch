fef99a57f2b9951d53ae2971d4fddff637e2235e
B00TK1D <stearns.josiah@gmail.com>
2023-09-14 16:06:30 -0400

    Removed old service

--- services/NorthernFiles/auth_proxy/src/go.mod
+++
@@ -1,5 +0,0 @@
-module main
-
-go 1.19
-
-require github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
--- services/NorthernFiles/frontend/src/routes/login/+page.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit'
-import type { PageLoad } from './$types'
-
-export const load: PageLoad = async ({ parent }) => {
-	const data = await parent()
-	if (data.session) {
-		throw redirect(302, '/dashboard')
-	}
-}
