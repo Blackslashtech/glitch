88402849d034742f39531029f1d449bfbb5b1b36
B00TK1D <stearns.josiah@gmail.com>
2023-09-20 01:46:45 -0400

    Cleaned up docker image deletion

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -28,15 +28,15 @@ SERVICE_LIST=$(echo $SERVICES | tr ',' '\n')
 # Loop from 1 to $TEAM_COUNT
 for TEAM_ID in $(seq 1 $TEAM_COUNT); do
     # Loop over every service
+    echo "Deleting team $TEAM_ID..."
     for SERVICE_NAME in $SERVICE_LIST; do
         dir="./services/$SERVICE_NAME"
         # If the file is a directory
         if [ -d "$dir" ]; then
-            # Generate a random root password
             HOSTNAME=$(echo "team$TEAM_ID-$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
             echo "Deleting $HOSTNAME..."
-            docker rm -f $HOSTNAME > /dev/null
-            docker rmi -f $HOSTNAME-service > /dev/null
+            docker rm -f $HOSTNAME > /dev/null 2>&1
+            docker rmi -f $HOSTNAME-service > /dev/null 2>&1
         fi
     done
 done
@@ -46,11 +46,11 @@ for SERVICE_NAME in $SERVICE_LIST; do
     dir="./checkers/$SERVICE_NAME"
     # If the file is a directory
     if [ -d "$dir" ]; then
-        # Generate a random root password
+        echo "Deleting $SERVICE_NAME checker..."
         HOSTNAME=$(echo "checker-$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
         echo "Deleting $HOSTNAME..."
-        docker rm -f $HOSTNAME > /dev/null
-        docker rmi -f $HOSTNAME-checker > /dev/null
+        docker rm -f $HOSTNAME > /dev/null 2>&1
+        docker rmi -f $HOSTNAME-checker > /dev/null 2>&1
     fi
 done
 
