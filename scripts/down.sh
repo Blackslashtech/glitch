cc8a8ca4627f5e20f733cbbfbfecf291ef6bb408
B00TK1D <stearns.josiah@gmail.com>
2023-09-20 01:39:35 -0400

    Removed duplicate checker runs in down.sh

--- scripts/down.sh
+++ scripts/down.sh
@@ -18,20 +18,19 @@ source .env set
 
 SERVICE_LIST=$(echo $SERVICES | tr ',' '\n')
 
+for SERVICE_NAME in $SERVICE_LIST; do
+    dir="./checkers/$SERVICE_NAME"
+    # If the file is a directory
+    if [ -d "$dir" ]; then
+        # Generate a random root password
+        HOSTNAME=$(echo "checker-$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
+        echo "Stopping $HOSTNAME..."
+        docker stop $HOSTNAME -t 1 > /dev/null &
+    fi
+done
+
 # Loop from 1 to $TEAM_COUNT
 for TEAM_ID in $(seq 1 $TEAM_COUNT); do
-    # Loop over every checker in /checkers
-    for SERVICE_NAME in $SERVICE_LIST; do
-        dir="./checkers/$SERVICE_NAME"
-        # If the file is a directory
-        if [ -d "$dir" ]; then
-            # Generate a random root password
-            HOSTNAME=$(echo "checker-$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
-            echo "Stopping $HOSTNAME..."
-            docker stop $HOSTNAME -t 1 > /dev/null &
-        fi
-    done
-    # Loop over every directory in /services
     for SERVICE_NAME in $SERVICE_LIST; do
         dir="./services/$SERVICE_NAME"
         # If the file is a directory
