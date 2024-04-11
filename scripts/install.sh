320b281a8e7e7a68bfbff9d80f1d246f27fc9a15
B00TK1D <stearns.josiah@gmail.com>
2024-04-11 15:34:03 -0400

    Added explicit zip dependancy

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 
 # Check if script is run as root
 if [ "$(id -u)" != "0" ]; then
-  echo "Please run this script as root (i.e. sudo sh scripts/install.sh)"
-  exit 1
+    echo "Please run this script as root (i.e. sudo sh scripts/install.sh)"
+    exit 1
 fi
 
 # Add Docker's official GPG key:
@@ -15,9 +15,9 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 
 # Install docker
@@ -27,6 +27,9 @@ echo -e '{ "experimental": true, "ip6tables": true, "dns" : [ "8.8.8.8" , "8.8.4
 
 systemctl enable docker
 systemctl restart docker
+
+# Install zip
+sudo apt-get install -y zip
 
 # Copy sample.env to .env if it doesn't already exist
 cp -n sample.env .env
